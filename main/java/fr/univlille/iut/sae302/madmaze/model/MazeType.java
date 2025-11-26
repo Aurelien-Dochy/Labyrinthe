cb28bfb3c9b773253a7f4c587e2caf4608e5e9c3
Aurelien-Dochy <151550747+Aurelien-Dochy@users.noreply.github.com>
2025-11-26 08:21:15 +0100

    Delete main directory

--- main/java/fr/univlille/iut/sae302/madmaze/model/MazeType.java
+++
@@ -1,6 +0,0 @@
-package main.java.fr.univlille.iut.sae302.madmaze.model;
-
-public enum MazeType {
-    RANDOM,
-    PERFECT
-}
--- main/java/fr/univlille/iut/sae302/madmaze/view/Observer.java
+++
@@ -1,7 +0,0 @@
-package main.java.fr.univlille.iut.sae302.madmaze.view;
-
-import main.java.fr.univlille.iut.sae302.madmaze.model.Observable;
-
-public interface Observer {
-    void update(Observable modele);
-}
